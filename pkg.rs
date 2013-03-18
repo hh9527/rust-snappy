f5040628b6a7d644c072a1da72de50061bc8c4fb
Daniel Micay <danielmicay@gmail.com>
2013-03-17 21:35:30 -0400

    port to ruskpkg

---
+++ pkg.rs
@@ -0,0 +1,2 @@
+#[pkg_crate(file = "snappy.rs")];
+#[pkg(id = "snappy", vers = "0.1.0")];
--- snappy.rs
+++ snappy.rs
@@ -1,5 +1,5 @@
 #[link(name = "snappy",
-       vers = "0.1",
+       vers = "0.1.0",
        uuid = "17d57f36-462f-49c8-a3e1-109a7a4296c8",
        url = "https://github.com/thestinger/rust-snappy")];
 
