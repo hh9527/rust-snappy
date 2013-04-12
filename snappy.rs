b3b02615a809287b925600a90a51b4d28493adb5
Daniel Micay <danielmicay@gmail.com>
2013-04-11 20:44:39 -0400

    no need for const

--- snappy.rs
+++ snappy.rs
@@ -11,19 +11,19 @@ use core::libc::{c_int, size_t};
 
 #[link_args = "-lsnappy"]
 extern "C" {
-    fn snappy_compress(input: *const u8,
+    fn snappy_compress(input: *u8,
                        input_length: size_t,
                        compressed: *mut u8,
                        compressed_length: *mut size_t) -> c_int;
-    fn snappy_uncompress(compressed: *const u8,
+    fn snappy_uncompress(compressed: *u8,
                          compressed_length: size_t,
                          uncompressed: *mut u8,
                          uncompressed_length: *mut size_t) -> c_int;
     fn snappy_max_compressed_length(source_length: size_t) -> size_t;
-    fn snappy_uncompressed_length(compressed: *const u8,
+    fn snappy_uncompressed_length(compressed: *u8,
                                   compressed_length: size_t,
                                   result: *mut size_t) -> c_int;
-    fn snappy_validate_compressed_buffer(compressed: *const u8,
+    fn snappy_validate_compressed_buffer(compressed: *u8,
                                          compressed_length: size_t) -> c_int;
 }
 
